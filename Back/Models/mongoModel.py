2311162d8aafab85eeb7105c2f01f15913f863c3
Bernardooq <bernax123321@gmail.com>
2024-11-23 21:02:44 -0600

    Mongo1

--- Back/Models/mongoModel.py
+++ Back/Models/mongoModel.py
@@ -8,14 +8,13 @@ def init_mongo(mongo_uri, db_name):
     return client, db
 
 # Insert User
-def add_mongo_user(mongodb_database, user_id, name, email, hashed_password):
+def add_mongo_user(mongodb_database, username, email, hashed_password):
     try:
         user = {
-            "user_id": user_id,
-            "name": name,
+            "username": username,
             "email": email,
             "hashed_password": hashed_password,
-            "registration_date": datetime.utcnow()  # Fecha de registro
+            "registration_date": datetime.now()  # Fecha de registro
         }
         mongodb_database.users.insert_one(user)
     except Exception as e:
@@ -23,39 +22,13 @@ def add_mongo_user(mongodb_database, user_id, name, email, hashed_password):
         raise
 
 # Get User
-def get_mongo_user(mongodb_database, user_id):
+def get_mongo_user(mongodb_database, email):
     try:
-        user = mongodb_database.users.find_one({"user_id": user_id})
+        user = mongodb_database.users.find_one({"email": email})
         return user
     except Exception as e:
         print(f"Error getting user from MongoDB: {e}")
         raise 
-
-# Purschase
-def add_purchase_order(mongodb_database, order_id, user_id, products, total_price, payment_method, order_status="Pending"):
-    try:
-        order = {
-            "order_id": order_id,
-            "user_id": user_id,
-            "products": products,  # Lista de diccionarios con productID, quantity, price
-            "total_price": total_price,
-            "payment_method": payment_method,
-            "order_status": order_status,
-            "order_date": datetime.utcnow()
-        }
-        mongodb_database.orders.insert_one(order)
-    except Exception as e:
-        print(f"Error inserting purchase order into MongoDB: {e}")
-        raise
-
-# User history
-def get_user_order_history(mongodb_database, user_id):
-    try:
-        orders = mongodb_database.orders.find({"user_id": user_id}).sort("order_date", -1)  # Ordenar por fecha descendente
-        return list(orders)
-    except Exception as e:
-        print(f"Error getting order history from MongoDB: {e}")
-        raise
 
 # Product catalog
 def add_product(mongodb_database, product_id, name, description, price, stock, category, image_url):
@@ -74,6 +47,20 @@ def add_product(mongodb_database, product_id, name, description, price, stock, c
         print(f"Error inserting product into MongoDB: {e}")
         raise
 
+def find_product(mongodb_database, product_id):
+    try:
+        product=mongodb_database.products.find_one({"product_id": product_id})
+        return product
+    except Exception as e:
+        print(f"Error inserting product into MongoDB: {e}")
+
+def update_product(mongodb_database, product_id, name, description, price, stock, category, image_url):
+    try:
+        product= mongodb_database.products.update_one({"product_id": product_id,}, 
+            {"$set": {"name": name, "description": description, "price": price, "stock": stock, "category" : category, "image_url": image_url}})
+        return product
+    except Exception as e:
+        print("Error updating product")
 # Search Products
 def search_products(mongodb_database, query):
     try:
@@ -85,11 +72,18 @@ def search_products(mongodb_database, query):
         print(f"Error searching products in MongoDB: {e}")
         raise
 
+def find_product(mongodb_database, product_id):
+    try:
+        product= mongodb_database.products.find_one({"product_id": product_id})
+        return(product)
+    except Exception as e:
+        print(f"Error finding product in MongoDB: {e}")
+
 # Add to shopping cart
-def add_to_cart(mongodb_database, user_id, product_id, product_name, quantity, price, description):
+def add_to_cart(mongodb_database, email, product_id, product_name, quantity, price, description):
     try:
         cart_item = {
-            "user_id": user_id,
+            "email": email, 
             "product_id": product_id,
             "product_name": product_name,
             "quantity": quantity,
@@ -102,21 +96,37 @@ def add_to_cart(mongodb_database, user_id, product_id, product_name, quantity, p
         raise
 
 # Get User shopping cart
-def get_user_cart(mongodb_database, user_id):
+def get_user_cart(mongodb_database, email):
     try:
-        cart_items = mongodb_database.carts.find({"user_id": user_id})
+        cart_items = mongodb_database.carts.find({"email": email})
         return list(cart_items)
     except Exception as e:
         print(f"Error getting user cart from MongoDB: {e}")
         raise
 
+def edit_user_cart(mongodb_database, email, new_cart):
+    try:
+         if new_cart["quantity"]==0:
+             mongodb_database.carts.delete_one({"email": email}, {"product_id": new_cart["product_id"]})
+        
+        
+         mongodb_database.carts.update_one(
+                {"email": email, "product_id": new_cart["product_id"]},  # Filtrar por email y product_id
+                {"$set": {
+                    "quantity": new_cart["quantity"],
+                }},
+                upsert=True  # Si el producto no existe, se inserta como nuevo
+            )
+    except Exception as e:
+        print(f"Error updating user cart from MongoDB: {e}")
+
 # Insert product at wishlist
-def add_to_wishlist(mongodb_database, user_id, product_id):
+def add_to_wishlist(mongodb_database, email, product_id):
     try:
         wishlist_item = {
-            "user_id": user_id,
+            "email": email,
             "product_id": product_id,
-            "date_added": datetime.utcnow()
+            "date_added": datetime.now()
         }
         mongodb_database.wishlist.insert_one(wishlist_item)
     except Exception as e:
@@ -124,37 +134,30 @@ def add_to_wishlist(mongodb_database, user_id, product_id):
         raise
 
 # Get user wishlist
-def get_user_wishlist(mongodb_database, user_id):
+def get_user_wishlist(mongodb_database, email):
     try:
-        wishlist_items = mongodb_database.wishlist.find({"user_id": user_id})
+        wishlist_items = mongodb_database.wishlist.find({"email": email})
         return list(wishlist_items)
     except Exception as e:
         print(f"Error getting user wishlist from MongoDB: {e}")
         raise
 
+def edit_user_wishlist(mongodb_database, email, new_wishlist):
+    try:
+        wishlist= mongodb_database.wishlist.update(new_wishlist)
+        return list(wishlist)
+    except Exception as e: 
+        print(f"Error updating user wishlist from MongoDB: {e}")
+
 # Update user info
-def update_user_info(mongodb_database, user_id, updated_info):
+def update_user_info(mongodb_database, email, updated_info):
     try:
         mongodb_database.users.update_one(
-            {"user_id": user_id},
+            {"email": email},
             {"$set": updated_info}
         )
     except Exception as e:
         print(f"Error updating user info in MongoDB: {e}")
-        raise
-
-# Product feedback
-def add_product_feedback(mongodb_database, user_id, product_id, feedback_text):
-    try:
-        feedback = {
-            "user_id": user_id,
-            "product_id": product_id,
-            "feedback_text": feedback_text,
-            "timestamp": datetime.utcnow()
-        }
-        mongodb_database.product_feedback.insert_one(feedback)
-    except Exception as e:
-        print(f"Error inserting product feedback in MongoDB: {e}")
         raise
 
 # Return request
@@ -165,18 +168,10 @@ def add_return_request(mongodb_database, order_id, product_id, reason):
             "product_id": product_id,
             "reason": reason,
             "status": "Pending",
-            "request_date": datetime.utcnow()
+            "request_date": datetime.now()
         }
-        mongodb_database.return_requests.insert_one(return_request)
+        mongodb_database.returns.insert_one(return_request)
     except Exception as e:
         print(f"Error inserting return request into MongoDB: {e}")
         raise
 
-# Order status
-def get_order_status(mongodb_database, order_id):
-    try:
-        order = mongodb_database.orders.find_one({"order_id": order_id})
-        return order["order_status"]
-    except Exception as e:
-        print(f"Error getting order status from MongoDB: {e}")
-        raise
